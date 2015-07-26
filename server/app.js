775148856a8def0d8ddd9ff3a590ea2f1e4266b5
MKS-students <mks.students@hackreactor.com>
2015-07-25 17:46:21 -0700

    [Progress] Setup the environment for mongoDB I/O.

--- server/app.js
+++ server/app.js
@@ -22,15 +22,13 @@ module.exports.app = app;
 // Set what we are listening on.
 app.set("port", 3000);
 
-// // Logging and parsing
- app.use(morgan('dev'));
- app.use(parser.json());
- app.use(parser.urlencoded({ extended: true }));
-// // Set up our routes
- app.use("/classes", router);
-
-// // Serve the client files
- app.use(express.static(__dirname + "/../client"));
+app.use(morgan('dev')); // Logging and parsing
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+// Set up our routes
+app.use("/classes", router);
+// Serve the client files
+app.use(express.static(__dirname + "/../client"));
 
 //Allow Cross Origin Requests to pass through
  app.use(function(req, res, next) {
@@ -39,7 +37,7 @@ app.set("port", 3000);
   next();
 });
 
-// // If we are being run directly, run the server.
+//If we are being run directly, run the server.
  if (!module.parent) {
    app.listen(app.get("port"));
    console.log("Listening on", app.get("port"));
